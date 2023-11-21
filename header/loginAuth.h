d67f57f8fe0144d650fc0dc5070800e127983c01
DarrenLiang27 <dlian032.ucr.edu>
2023-11-21 00:11:25 -0800

    Add change to top of header class for loginAuth

--- header/loginAuth.h
+++ header/loginAuth.h
@@ -1,5 +1,5 @@
-#ifndef SONG_H
-#define SONG_H
+#ifndef LOGINAUTH_H
+#define LOGINAUTH_H
 
 #include <iostream>
 
@@ -29,7 +29,7 @@ class loginAuth{
     void setBoolUser(bool check);
 
     /*Display MSG*/
-    
+
 };
 
 #endif
