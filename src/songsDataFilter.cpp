60fd1dbdb0ce1c87d5c301bcbb627460cb7d3d80
roshinira123 <rrang020@xe-01.cs.ucr.edu>
2023-12-08 10:34:05 -0800

    Added Aheli's change

--- src/songsDataFilter.cpp
+++ src/songsDataFilter.cpp
@@ -1,25 +1,33 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 #include <iostream>
+#include <random>
 #include <string>
 #include "../header/songsDataManager.h"
 #include "../header/song.h"
 #include "../header/songsDataFilter.h"
+
+//  /***/ at the end of a line means line was newly added
 
 void songsDataFilter :: getSongsByGenre(const string &genre) //select 5 songs from user's genre and add them to songsByGenre vector
 {
     songsDataManager sDM;
     sDM.GetData("Data/SpotifyFeatures - SpotifyFeatures.csv");
     vector<Song> dbSongs = sDM.getSongs();
+    shuffle(dbSongs.begin(), dbSongs.end(), random_device()); /***/
 
+    bool songOfGenreFound = true; /***/
     int genreCount = 0;
 
     for (int i = 0; i < dbSongs.size(); ++i)
     {
-        if (dbSongs.at(i).getGenre()== genre)
+        if (dbSongs.at(i).getGenre() == genre && dbSongs.at(i).getRecommendation() == false) /***/
         {
+            songOfGenreFound = true; /***/
             ++genreCount;
+            dbSongs.at(i).changeRecommendation(); /***/ //song is now set to recommended
             SongsByGenre.push_back(dbSongs.at(i));
 
             if (genreCount == 5) // songsByGenre now has 5 songs from a specific genre
@@ -27,7 +35,17 @@ void songsDataFilter :: getSongsByGenre(const string &genre) //select 5 songs fr
                 break;
             }
         }
+        else /***/
+        {
+            songOfGenreFound = false; /***/
+        } /***/
     }
+
+    if (songOfGenreFound == false) /***/
+    {
+        cout << endl; /***/
+        cout << "We could not generate songs for the genre " << "\"" << genre << ".\"" << endl; /***/
+    } /***/
 }
 
 void songsDataFilter :: getSongsByArtist(const string &artist) //select 5 songs from user's artist and add them to songsByArtist vector
@@ -35,14 +53,18 @@ void songsDataFilter :: getSongsByArtist(const string &artist) //select 5 songs 
     songsDataManager sDM;
     sDM.GetData("Data/SpotifyFeatures - SpotifyFeatures.csv");
     vector<Song> dbSongs = sDM.getSongs();
+    shuffle(dbSongs.begin(), dbSongs.end(), random_device()); /***/
 
+    bool songOfArtistFound = true; /***/
     int artistCount = 0;
 
     for (int i = 0; i < dbSongs.size(); ++i)
     {
-        if (dbSongs.at(i).getArtist() == artist)
+        if (dbSongs.at(i).getArtist() == artist && dbSongs.at(i).getRecommendation() == false) /***/
         {
+            songOfArtistFound = true; /***/
             ++artistCount;
+            dbSongs.at(i).changeRecommendation(); /***/ //song is now set to recommended
             SongsByArtist.push_back(dbSongs.at(i));
 
             if (artistCount == 5) // songsByArtist now has 5 songs from a specific artist
@@ -50,7 +72,16 @@ void songsDataFilter :: getSongsByArtist(const string &artist) //select 5 songs 
                 break;
             }
         }
+        else /***/
+        {
+            songOfArtistFound = false; /***/
+        } /***/
     }
+
+    if (songOfArtistFound == false) /***/
+    {
+        cout << "We could not generate songs from the artist " << "\"" << artist << ".\"" << endl; /***/
+    } /***/
 }
 
 void songsDataFilter :: getSongsByMood(const string &mood)
@@ -58,22 +89,35 @@ void songsDataFilter :: getSongsByMood(const string &mood)
     songsDataManager sDM;
     sDM.GetData("Data/SpotifyFeatures - SpotifyFeatures.csv");
     vector<Song> dbSongs = sDM.getSongs();
+    shuffle(dbSongs.begin(), dbSongs.end(), random_device()); /***/
 
+    bool songOfMoodFound = true; /***/
     int moodCount = 0;
 
     for (int i = 0; i < dbSongs.size(); ++i)
     {
-        if (dbSongs.at(i).getMood() == (mood))
+        if (dbSongs.at(i).getMood() == mood && dbSongs.at(i).getRecommendation() == false) /***/
         {
+            songOfMoodFound = true; /***/
             ++moodCount;
+            dbSongs.at(i).changeRecommendation(); /***/ //song is now set to recommended
             SongsByMood.push_back(dbSongs.at(i));
 
-            if (moodCount == 5)
+            if (moodCount == 5) // songsByArtist now has 5 songs of a specific mood
             {
                 break;
             }
         }
+        else /***/
+        {
+            songOfMoodFound = false; /***/
+        } /***/
     }
+
+    if (songOfMoodFound == false) /***/
+    {
+        cout << "We could not generate songs for the mood " << "\"" << mood << ".\"" << endl; /***/
+    } /***/
 }
 
 vector<Song> songsDataFilter :: personalizedSongList()
@@ -87,10 +131,11 @@ vector<Song> songsDataFilter :: personalizedSongList()
     SongsByGenre = tempSongsByGenre;
 
     return personalizedSongs;
-} 
-void songsDataFilter::clear() {
+}
+
+void songsDataFilter :: clear()
+{
     SongsByGenre.clear();
     SongsByArtist.clear();
     SongsByMood.clear();
 }
-
